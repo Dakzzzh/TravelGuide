08e4b66b4c2d417b090ac4ad8719d05489fb71ae
Dakzzzh <154968283+Dakzzzh@users.noreply.github.com>
2025-07-18 19:07:54 -0700

    Delete frontend directory

--- frontend/src/frontendComponents/VideoComponent.jsx
+++
@@ -1,9 +0,0 @@
-const VideoComponent = () => {
-    return (
-        <video loop autoPlay='true' muted className="myVideo">
-            <source src={('../assets/backgroundVideo.mp4')} type="video/mp4" />
-        </video>
-    );
-}
-
-export default VideoComponent;
